226b83527d5097eb987f9040b33702ea0bd08101
Eduardo Firvida <efirvida@gmail.com>
2025-02-06 13:37:37 +0000

    Add crm_socials addon
    An addod to include social networks to CRM Customers

---
+++ extra-addons/crm_socials/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Social links for CRM",
+    "version": "1.0",
+    "category": "Sales/CRM",
+    "summary": "Add social links to CRM Customers",
+    "description": "",
+    "author": "Eduardo Miguel Firvida Donestevez",
+    "license": "GPL-3",
+    "depends": ["crm"],
+    "data": ["views/res_partner_views.xml"],
+    "assets": {},
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
---
+++ extra-addons/crm_socials/models/res_partner.py
@@ -0,0 +1,29 @@
+import validators as validate
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class SocialLinks(models.Model):
+    _inherit = "res.partner"
+
+    facebook_url = fields.Char(string="Facebook URL", translate=True)
+    linkedin_url = fields.Char(string="LinkedIn URL", translate=True)
+    x_url = fields.Char(string="X URL", translate=True)
+    is_profile_completed = fields.Boolean(compute="_compute_profile_completed")
+
+    @api.depends("facebook_url", "linkedin_url", "x_url")
+    def _compute_profile_completed(self) -> None:
+        for record in self:
+            record.is_profile_completed = all(
+                [record.facebook_url, record.linkedin_url, record.x_url]
+            )
+
+    @api.constrains("facebook_url", "linkedin_url", "x_url")
+    def _check_social_urls(self) -> None:
+        for record in self:
+            if record.facebook_url and not validate.url(record.facebook_url):
+                raise ValidationError(_("Invalid Facebook URL"))
+            if record.linkedin_url and not validate.url(record.linkedin_url):
+                raise ValidationError(_("Invalid LinkedIn URL"))
+            if record.x_url and not validate.url(record.x_url):
+                raise ValidationError(_("Invalid X URL"))
