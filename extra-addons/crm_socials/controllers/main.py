3ffcd9792c032c682ae0bd2c08727af78e7f946a
Eduardo Firvida <efirvida@gmail.com>
2025-02-06 17:48:34 +0000

    Add customers search in website

--- extra-addons/crm_socials/controllers/main.py
+++ extra-addons/crm_socials/controllers/main.py
@@ -9,3 +9,24 @@ class WebsiteCustomers(http.Controller):
         return request.render(
             "crm_socials.customers_template", {"customers": customers}
         )
+
+    @http.route("/search", type="http", auth="public", website=True)
+    def search_customers(self, **kw):
+        search_query = kw.get("q", "")
+
+        domain = [
+            "|",
+            ("name", "ilike", search_query),
+            "|",
+            ("facebook_url", "ilike", search_query),
+            "|",
+            ("linkedin_url", "ilike", search_query),
+            ("x_url", "ilike", search_query),
+        ]
+
+        customers = request.env["res.partner"].search(domain)
+
+        return request.render(
+            "crm_socials.customers_template",
+            {"customers": customers, "query": search_query},
+        )
